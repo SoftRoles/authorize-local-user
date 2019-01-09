66aec84f87f3b7cbb0f019c3e7cd8901ebb46890
Hüseyin YİĞİT (BILGEM) <huseyin.yigit@tbtk.gov.tr>
2019-01-09 09:56:27 +0300

    simplify local user grant

--- test/index.js
+++ test/index.js
@@ -8,10 +8,10 @@ var app = express();
 const ensureLogin = require("../lib/index").ensureLogin
 
 app.use(require('morgan')('tiny'));
-app.use(ensureLogin({redirectTo:"/403"}))
+app.use(ensureLogin({redirectTo:"/403", localUser: true}))
 
 app.get("/", function(req,res){
-    res.send("HeLlo world!")
+    res.send("Hello world!")
 })
 app.get("/403", function(req,res){
     res.sendStatus(403)
@@ -19,6 +19,6 @@ app.get("/403", function(req,res){
 
 freePort(3000, function(err, port){
     app.listen(port, function() {
-        console.log("Service running on http://127.0.0.1:",port)
+        console.log("Service running on http://127.0.0.1:"+port)
     })
 })
